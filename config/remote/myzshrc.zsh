776ef1f19e19cb89e4a7f0de8982d6d613be4060
Shura Vlasov <superkoder@gmail.com>
2026-05-14 16:05:01 +0300

    chore: remove extra remote aliases

--- config/remote/myzshrc.zsh
+++ config/remote/myzshrc.zsh
@@ -1,9 +1,6 @@
 alias e="nano"
 alias rcs="source ~/.zshrc"
 alias rce="e ~/.zshrc && rcs"
-alias purge="sudo apt autoremove --purge && sudo apt autoclean && sudo journalctl --vacuum-time=7d"
-alias dstats="docker stats --no-stream"
-alias xe="e /usr/local/x-ui/bin/config.json"
 
 setopt prompt_subst
 
